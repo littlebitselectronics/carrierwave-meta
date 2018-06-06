c1289d2b52fa8ae51d5e6db7abb8276756007d8f
Mihai Târnovan <mihai.tarnovan@gmail.com>
2018-06-06 10:07:49 +0300

    Remove call to #set_content_type

--- lib/carrierwave-meta/meta.rb
+++ lib/carrierwave-meta/meta.rb
@@ -8,11 +8,11 @@ module CarrierWave
     included do
       include CarrierWave::ModelDelegateAttribute
 
-      set_content_type(true)
+      # set_content_type(true)
 
-      after :retrieve_from_cache, :set_content_type
+      # after :retrieve_from_cache, :set_content_type
       after :retrieve_from_cache, :call_store_meta
-      after :retrieve_from_store, :set_content_type unless storage.name =~ /Fog/
+      # after :retrieve_from_store, :set_content_type unless storage.name =~ /Fog/
       after :retrieve_from_store, :call_store_meta
 
       model_delegate_attribute :content_type, ''
